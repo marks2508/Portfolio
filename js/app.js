67b74d8c8d3a9581d23a5eba1e0c4d6ef9710df2
Mark S <markscicluna@hotmail.com>
2018-03-31 17:40:57 +0100

    added image caption over the top

--- js/app.js
+++ js/app.js
@@ -1,17 +1,3 @@
 $(() => {
-  $(function(){
 
-    var imp = impress();
-
-    $('#arrowLeft').click(function(e){
-      imp.prev();
-      e.preventDefault();
-    });
-
-    $('#arrowRight').click(function(e){
-      imp.next();
-      e.preventDefault();
-    });
-
-  });
 });
