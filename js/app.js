32cf9df14b5d64de712bb46d4dbe7990ba3c3779
Mark S <markscicluna@hotmail.com>
2018-04-02 11:34:15 +0100

    fixed sections height

--- js/app.js
+++ js/app.js
@@ -57,4 +57,23 @@ $(() => {
     css.innerHTML = '.typewrite > .wrap { border-right: 0.08em solid #fff}';
     document.body.appendChild(css);
   };
+
+  $('#nav ul li a[href^=#]').on('click', function(e) {
+
+    // prevent default anchor click behavior
+    e.preventDefault();
+
+    // animate
+    $('html, body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 300, function(){
+
+      // when done, add hash to url
+      // (default click behaviour)
+      window.location.hash = this.hash;
+    });
+
+  });
+
+
 });
